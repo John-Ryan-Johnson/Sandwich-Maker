d39e0efcb7484c1f9caaae6a1c52adc6202fd284
John-Ryan-Johnson <john4johnkids@gmail.com>
2019-09-25 20:54:17 -0500

    trying to print to dom

---
+++ javascripts/components/cheese.js
@@ -0,0 +1,42 @@
+import utilities from '../helpers/utilities.js'
+
+
+const cheeses = [
+  {id:"cheese1", name:"swiss", price: .50},
+  {id:"cheese2", name:"mozzarella", price: .60},
+  {id:"cheese3", name:"monterey", price: .90},
+  {id:"cheese4", name:"parmesan", price: .40},
+  {id:"cheese5", name:"cheddar", price: .75}
+];
+
+const getSelectedCheeses = () => {
+  const selectedCheeses = [];
+  const cheeseCheckboxes = document.getElementsByClassName('cheese');
+    for(let j = 0; j < cheeseCheckboxes.length; j++) {
+      for(let k = 0; k < cheeses.length; k++){
+        if(cheeseCheckboxes[j].checked && cheeseCheckboxes[j].id === cheeses[k].id){
+          selectedCheeses.push(cheeses[k]);
+        }
+      }
+    }
+       return selectedCheeses;
+  
+};
+
+
+const printCheeseOptions = () => {
+  let domString = '';
+  for(let i = 0; i < cheeses.length; i++){
+     domString +=`<div class="form-group form-check">
+    <input type="checkbox" class="form-check-input cheese" id=${cheeses[i].id}>
+    <label class="form-check-label" for=${cheeses[i].id}">${cheeses[i].name}</label>
+  </div>`
+  }
+  
+ 
+
+  utilities.printToDom('cheese-counter',domString )
+};
+
+
+export default { printCheeseOptions, getSelectedCheeses };
---
+++ javascripts/components/meat.js
@@ -0,0 +1,41 @@
+import utilities from '../helpers/utilities.js'
+
+const meats = [
+  {id:"meat1", name:"turkey", price:"3.00"},
+  {id:"meat2", name:"ham", price:"3.00"},
+  {id:"meat3", name:"chicken", price:"3.00"},
+  {id:"meat4", name:"tuna", price:"2.00"},
+  {id:"meat5", name:"roast beef", price:"3.50"}
+];
+
+const getSelectedMeats = () => {
+  const selectedMeats = [];
+  const meatCheckboxes = document.getElementsByClassName('meat');
+    for(let j = 0; j < meatCheckboxes.length; j++) {
+      for(let k = 0; k < meats.length; k++){
+        if(meatCheckboxes[j].checked && meatCheckboxes[j].id === meats[k].id){
+          selectedMeats.push(meats[k]);
+        }
+      }
+    }
+       return selectedMeats;
+  
+};
+
+
+const printMeatOptions = () => {
+  let domString = '';
+  for(let i = 0; i < meats.length; i++){
+     domString +=`<div class="form-group form-check">
+    <input type="checkbox" class="form-check-input meat" id=${meats[i].id}>
+    <label class="form-check-label" for=${meats[i].id}">${meats[i].name}</label>
+  </div>`
+  }
+  
+ 
+
+  utilities.printToDom('meat-counter',domString )
+};
+
+
+export default { printMeatOptions, getSelectedMeats };
